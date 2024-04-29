8cd96dcb1beab65c6410ed085bb2063e4d275c3e
Zaeem Sheikh <124173573+SheikhZaeem@users.noreply.github.com>
2024-04-29 21:48:33 +0300

    Delete UniversitySync directory

--- UniversitySync/src/app/AppRunner.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-public class AppRunner {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
--- UniversitySync/src/app/Reviewable.java
+++
@@ -1,7 +0,0 @@
-package app;
-
-import app.userContent.Review;
-
-public interface Reviewable {
-    void addReview(Review rev);
-}
