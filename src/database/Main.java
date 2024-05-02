a154dd16268b044fd4031325db6be4918d2b141f
eardaege <145162705+eardaege@users.noreply.github.com>
2024-05-02 13:31:20 +0300

    started DAO implementation
    added a DBConnectionManager that creates the connection. Added UserDAO class that can insert lines into User table based on the input given to its method. Added a Main file to be able to test this feature. Note that this is the very beginning of DAO implementation and also we need to declare packages as it should be in the main project. This DAO also assumes that you have a valid table initialized by the .sql script in this folder and also a valid connection.

---
+++ src/database/Main.java
@@ -0,0 +1,31 @@
+package com.example.dao; // TODO: be sure to correct this!
+
+import java.sql.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String url = "jdbc:mysql://localhost:3306/university_sync";
+        String username = "root";
+        String password = "ben123**AA"; // TODO: type your own mysql server password here!
+        /*
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement st = con.createStatement();
+        
+        ResultSet students = st.executeQuery("SELECT * FROM university_sync.student");
+        while (students.next()){
+            System.out.print(students.getInt("id"));
+            System.out.print(" " + students.getString("full_name"));
+            System.out.print(" " + students.getString("username"));
+            System.out.print(" " + students.getString("pass"));
+            System.out.print(" " + students.getString("biography"));
+            System.out.println(" " + students.getString("major"));
+            
+        }*/
+
+        DBConnectionManager.initializeConnection(url, username, password);
+        UserDAO.addUser("arda", 2);
+    }
+}
---
+++ src/database/UserDAO.java
@@ -0,0 +1,21 @@
+package com.example.dao; //TODO: be sure to correct this
+
+import java.sql.*;
+
+public class UserDAO {
+
+    public static boolean addUser(String name, int majorID){
+        try{
+            String query = "INSERT INTO university_sync.student (student_name, student_major_id) VALUES (?, ?);";
+            PreparedStatement pst = DBConnectionManager.getConnection().prepareStatement(query);
+            pst.setString(1, name);
+            pst.setInt(2, majorID);
+            pst.executeUpdate();
+        }
+        catch (SQLException sqle){
+            sqle.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
