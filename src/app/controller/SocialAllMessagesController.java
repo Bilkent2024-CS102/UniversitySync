6b8e329d27ba012e614b2ea520b6e0e7fd0a5888
ZaeemSheikh <124173573+SheikhZaeem@users.noreply.github.com>
2024-05-06 22:14:12 +0300

    Created Message Text box
    This is the place where user can send or receive messages from his/her friends

---
+++ src/app/controller/SocialAllMessagesController.java
@@ -0,0 +1,28 @@
+package app.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
+public class SocialAllMessagesController {
+    private FXMLLoader fxmlLoader;
+    @FXML
+    private BorderPane borderTextBox_ID;
+
+    public void displayTextScreen(MouseEvent event) throws IOException {
+           fxmlLoader = new FXMLLoader(new File("src/app/view/SocialPage/messageBox.fxml").toURI().toURL());
+           Pane newScene = fxmlLoader.load();
+           borderTextBox_ID.setCenter(newScene);
+    }
+
+}
