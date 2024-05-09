7a3203fef2013f4081bb5957106f6de7c4070255
ZaeemSheikh <124173573+SheikhZaeem@users.noreply.github.com>
2024-05-09 20:42:54 +0300

    Figured UI Connection
    For now using a sample Post class to display and dynamic ui that we will later use with database

--- src/app/controller/PostController.java
+++ src/app/controller/PostController.java
@@ -1,12 +1,13 @@
 package app.controller;
 
-import app.dao.UserDao;
-import app.model.User;
-import app.model.userContent.post.ForumPost;
+//import app.dao.UserDao;
+//import app.model.User;
+//import app.model.userContent.post.ForumPost;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
@@ -33,11 +34,40 @@ public class PostController {
     @FXML
     private TextArea postTextAreaID;
 
-    @FXML
-    private VBox postVBoxID;
 
-    public void setData(ForumPost post) {
-//        User user = UserDao.getUserByID(post.getOwnerId());
+    public void setData(Post post) {
+        //Image image = new Image(getClass().getResourceAsStream(post.getProfileImageSrc()));
+       // userImageOnPostID.setImage(image);
+        postUsernameID.setText(post.getUsername());
+        postTextAreaID.setText(post.getPostText());
+    }
+}
+class Post {
+    private String username;
+    private String postText;
+    private String profileImageSrc;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getProfileImageSrc() {
+        return profileImageSrc;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setProfileImageSrc(String profileImageSrc) {
+        this.profileImageSrc = profileImageSrc;
+    }
+
+    public void setPostText(String postText) {
+        this.postText = postText;
     }
 }
--- src/app/controller/TopTabController.java
+++ src/app/controller/TopTabController.java
@@ -1,22 +1,74 @@
 package app.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
 
 
-public class TopTabController {
+public class TopTabController implements Initializable  {
         private Stage stage;
         private Scene scene;
         private FXMLLoader fxmlLoader;
 
+// ******************************************************************* TEST ***************************************
+        @FXML
+        private VBox postVBoxID = new VBox();
+        private List<Post> posts;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        posts = new ArrayList<>(data());   //each time test controller class is called, we make a new arraylist and display the data
+
+        try {
+        for(int i = 0; i < posts.size(); i++) {
+
+            fxmlLoader = new FXMLLoader(new File("src/app/view/Post.fxml").toURI().toURL());
+
+            //fxmlLoader.setLocation(getClass().getResource());
+
+            Pane postPane = null;  //the pane that contains posts in the post fxml
+            postPane = fxmlLoader.load();
+
+            PostController postController = fxmlLoader.getController();
+            postController.setData(posts.get(i));
+
+            //npw adding post to the vbox
+            postVBoxID.getChildren().add(postPane);
+        }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private List<Post> data() {
+        List<Post> ls = new ArrayList<>();
+        Post post = new Post();
+        post.setUsername("Musa");
+        post.setPostText("hello i am musa and");
+        ls.add(post);
+
+        Post post2 = new Post();
+        post2.setUsername("Atilla");
+        post2.setPostText("hello i am atilla and");
+        ls.add(post2);
+        return ls;
+    }
+
+//    ******************************************************************* TEST ***************************************
     private void switchToFXML(String fxmlFileName, ActionEvent event) throws IOException {
         fxmlLoader = new FXMLLoader(new File(fxmlFileName).toURI().toURL());
         Parent root = fxmlLoader.load();
