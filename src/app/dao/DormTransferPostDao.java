33be4c35947d005d1531c821561116de9e0aa975
Atllkks10 <atiakkus10@gmail.com>
2024-05-10 20:05:36 +0300

    DormTransferPostDao Completed With Javadocs.

--- src/app/dao/DormTransferPostDao.java
+++ src/app/dao/DormTransferPostDao.java
@@ -9,20 +9,31 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class DormTransferPostDao {
+
+    /**
+     * Takes integer id and makes a 'SELECT' query.
+     * @param id
+     * @return the DormTransferPost instance by given id from database.
+     */
     public static DormTransferPost getPostById(int id)
     {
         DormTransferPost post = null;
         try
         {
-            String query = "SELECT * FROM university_sync.dorm_transfer_post WHERE dorm_transfer_post_id= ? ";
+            String query = "SELECT * FROM university_sync.dorm_transfer_post WHERE dorm_transfer_post_id=? ";
             PreparedStatement st = DBConnectionManager.getConnection().prepareStatement(query);
             st.setInt(1, id);
             ResultSet rs = st.executeQuery();
             rs.next();
-            post = new DormTransferPost(rs.getInt("dorm_transfer_post_id"), rs.getInt("owner_student_id"),
+            post = new DormTransferPost(
+                    rs.getInt("dorm_transfer_post_id"),
+                    rs.getInt("owner_student_id"),
                     rs.getString("main_text"),
-                    rs.getDate("creation_date"), rs.getDate("last_edit_date"),
-                    rs.getString("heading"), rs.getInt("posted_room_id"));
+                    rs.getDate("creation_date"),
+                    rs.getDate("last_edit_date"),
+                    rs.getString("heading"),
+                    rs.getInt("posted_room_id")
+            );
             return post;
         } catch (Exception sqle) {
             sqle.printStackTrace();
@@ -30,13 +41,20 @@ public class DormTransferPostDao {
         }
     }
 
-    public static boolean delete(int postId)
+    /**
+     * Deletes from the dorm_transfer_post table if possible.
+     * The post to be deleted is indicated by the given integer id.
+     * @param id
+     * @return if the deletion is done or not. It cannot be done if
+     * the given id doesn't correspond to any instance in the table.
+     */
+    public static boolean delete(int id)
     {
         try
         {
             String query = "DELETE FROM university_sync.dorm_transfer_post WHERE dorm_transfer_post_id= ?";
             PreparedStatement ps = DBConnectionManager.getConnection().prepareStatement(query);
-            ps.setInt(1, postId);
+            ps.setInt(1, id);
             ps.executeUpdate();
             return true;
         }
@@ -47,14 +65,21 @@ public class DormTransferPostDao {
         }
     }
 
-    public static boolean edit(int postId, String editedText)
+    /**
+     * Edit the main text of the dorm_transfer_post identified
+     * by the given integer id. New main_text will be {@code editedText}
+     * @param id
+     * @param editedText
+     * @return if the edit was successful or not.
+     */
+    public static boolean edit(int id, String editedText)
     {
         String query = "UPDATE university_sync.forum_post SET main_text= ? WHERE forum_post_id= ?";
         try
         {
             PreparedStatement pst = DBConnectionManager.getConnection().prepareStatement(query);
             pst.setString(1, editedText);
-            pst.setInt(2, postId);
+            pst.setInt(2, id);
             pst.executeQuery();
             return true;
         }
@@ -64,11 +89,65 @@ public class DormTransferPostDao {
         }
     }
 
-    public static int addDormTransferPost(DormTransferPost d){
-        return -1;
+    /**
+     * Adds the given DormTransferPost object to database.
+     * @param post
+     * @return whether the adding operation was successful or not.
+     */
+    public static int addDormTransferPost(DormTransferPost post)
+    {
+        String query = "INSERT INTO university_sync.dorm_transfer_post (dorm_transfer_post_id, owner_student_id, creation_date, last_edit_date, heading, main_text, posted_room_id) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement pst = DBConnectionManager.getConnection().prepareStatement(query);
+            pst.setInt(1, post.getUserContentItemId());
+            pst.setInt(2, post.getOwnerId());
+            pst.setDate(3, new Date(post.getCreationDate().getTime()));
+            pst.setDate(4, new Date(post.getLastEditDate().getTime()));
+            pst.setString(5, post.getHeading());
+            pst.setString(6, post.getMainText());
+            pst.setInt(7, post.getRoomId());
+            pst.executeUpdate();
+            return post.getUserContentItemId();
+        } catch (Exception sqle) {
+            sqle.printStackTrace();
+            return -1;
+        }
     }
 
-    public static ArrayList<DormTransferPost> getDormTransferPostOfRoomType(int roomTypeId){
-        return null;
+    /**
+     * Selects the {@code DormTransferPost} instances of the given room type,
+     * which is indicated by integer id. 
+     * @param roomTypeId
+     * @return the ArrayList of {@code DormTransferPost} instances
+     * with the given room type id.
+     */
+    public static ArrayList<DormTransferPost> getDormTransferPostOfRoomType(int roomTypeId)
+    {
+        ArrayList<DormTransferPost> posts = new ArrayList<DormTransferPost>();
+        DormTransferPost post = null;
+        try
+        {
+            String query = "SELECT * FROM university_sync.dorm_transfer_post WHERE posted_room_id=? ";
+            PreparedStatement st = DBConnectionManager.getConnection().prepareStatement(query);
+            st.setInt(1, roomTypeId);
+            ResultSet rs = st.executeQuery();
+            while(rs.next())
+            {
+                post = new DormTransferPost(
+                        rs.getInt("dorm_transfer_post_id"),
+                        rs.getInt("owner_student_id"),
+                        rs.getString("main_text"),
+                        rs.getDate("creation_date"),
+                        rs.getDate("last_edit_date"),
+                        rs.getString("heading"),
+                        rs.getInt("posted_room_id")
+                );
+                posts.add(post);
+            }
+            return posts;
+        } catch (Exception sqle) {
+            sqle.printStackTrace();
+            return null;
+        }
     }
 }
