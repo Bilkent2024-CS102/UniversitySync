8c3b871945c63e0b352ba5cbbe5987f66c0b7192
eardaege <ardaegeerdogan@gmail.com>
2024-05-10 14:38:23 +0300

    added UserDao methods and fixed some errors
    added updateUser and removeFriend methods in UserDao
    added major and biography fields to User
    prevent syntax errors by typecasting e.getSource(), however this requires more attention probably
    fixed method calls and parameters (refernce by id vs reference by actual object)

--- src/app/controller/DormitoryTransferController.java
+++ src/app/controller/DormitoryTransferController.java
@@ -3,10 +3,16 @@ package app.controller;
 import java.sql.Date;
 import java.time.LocalDate;
 
+import app.dao.UserDao;
+import app.model.User;
+import app.model.location.Room;
 import app.model.userContent.post.DormTransferPost;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+
+import app.dao.DormTransferPostDao;
+import app.dao.DormitoryDao;
 
 public class DormitoryTransferController {
     
@@ -20,33 +26,32 @@ public class DormitoryTransferController {
         String postDetails = dormDescription.getText();
         String postHeading = heading.getText();
         Date date = Date.valueOf(LocalDate.now());
-        DormTransferPost post = new DormTransferPost(SessionManager.getCurrentUser(), postDetails, date, date, postHeading, SessionManager.getCurrentUser().getRoom());
-        DormitoryPostDao.addPost(post);
+        DormTransferPost post = new DormTransferPost(SessionManager.getCurrentUser().getUserId(), postDetails, date, date, postHeading, SessionManager.getCurrentUser().getRoom().getRoomId());
     }
 
     public void openPost(ActionEvent e)
     {
-        DormitoryTransferPost post = e.getSource();
+        DormTransferPost post = (DormTransferPost) e.getSource();
         // need to make UI element for post here
     }
 
     public void messageDormOwner(ActionEvent e)
     {
-        DormitoryTransferPost post = e.getSource();
-        User dormOwner = post.getOwner();
+        DormTransferPost post = (DormTransferPost) e.getSource();
+        int dormPostOwnerId = post.getOwnerId();
         // UI method to open messages
     }
 
     public void transferDorm(ActionEvent e)
     {
-        User user1 = e.getSource().getOwner();
+        User whoPosted = UserDao.getUserById(((DormTransferPost)e.getSource()).getOwnerId());
         Room room1 = whoPosted.getRoom();
         User current = SessionManager.getCurrentUser();
         Room room2 = current.getRoom();
 
         whoPosted.setRoom(room2);
         current.setRoom(room1);
-        DormitoryDao.update(whoPosted);
-        DormitoryDao.update(current);
+        UserDao.updateUser(whoPosted);
+        UserDao.updateUser(current);
     }
 }
