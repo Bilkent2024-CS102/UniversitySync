6ea4c370558ae1006b88b123479126a2abef8f89
ZaeemSheikh <124173573+SheikhZaeem@users.noreply.github.com>
2024-05-11 15:53:45 +0300

    Create Post Popup
    updated post controller, css style, and some minor changes

--- src/app/controller/PostController.java
+++ src/app/controller/PostController.java
@@ -3,16 +3,30 @@ package app.controller;
 //import app.dao.UserDao;
 //import app.model.User;
 //import app.model.userContent.post.ForumPost;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class PostController {
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader fxmlLoader;
+
     @FXML
     private ImageView userImageOnPostID;
 
@@ -34,6 +48,26 @@ public class PostController {
     @FXML
     private TextArea postTextAreaID;
 
+    private void switchToFXML(String fxmlFileName, ActionEvent event) throws IOException {
+        fxmlLoader = new FXMLLoader(new File(fxmlFileName).toURI().toURL());
+        Parent root = fxmlLoader.load();
+
+        if (event.getSource() instanceof Node) {                                    //FOR Buttons
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        }
+        else if (event.getSource() instanceof MenuItem menuItem)  {                 //FOR MenuButtons
+            stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+        }
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setFullScreen(true);     //it should be after stage.setScene
+        stage.show();
+    }
+
+
+    public void switchToPostReply(ActionEvent event) throws IOException {
+        switchToFXML("src/app/view/SocialPage/.fxml", event);
+    }
 
 //   public void setData(Post post) {
 ////        User user = UserDao.getUserByID(post.getOwnerId());
