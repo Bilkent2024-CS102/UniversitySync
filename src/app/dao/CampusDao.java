d98746304b6a0e056b52edda3c240452a99cd80c
Berkay <146650666+berkaydemircin@users.noreply.github.com>
2024-05-13 22:01:33 +0300

    Added CampusDao and getForumPostsOfUser

---
+++ src/app/dao/CampusDao.java
@@ -0,0 +1,36 @@
+package app.dao;
+
+import app.model.Campus;
+import app.model.User;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CampusDao{
+
+    /**
+     * Returns a campus object from db with ID
+     *
+     * @param Id id of the campus desired
+     * @return Campus matching with id input
+     */
+    public static Campus getCampusById(int Id)
+    {
+        try
+        {
+            String query = "SELECT * FROM university_sync.campus WHERE campus_id = ?;";
+            PreparedStatement pst = DBConnectionManager.getConnection().prepareStatement(query);
+            pst.setInt(1, Id);
+            ResultSet rs = pst.executeQuery();
+            rs.next();
+            Campus c = new Campus(rs.getInt("campus_id"), rs.getString("campus_name"), null, null);
+            return c;
+        }
+        catch (SQLException sqle)
+        {
+            sqle.printStackTrace();
+            return null;
+        }
+    }
+}
