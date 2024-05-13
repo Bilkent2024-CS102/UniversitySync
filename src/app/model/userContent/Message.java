0744c6ddfce19c7bc15765e642b8ca0f881883ab
eardaege <ardaegeerdogan@gmail.com>
2024-05-13 21:17:02 +0300

    update Message related things
    added idless and id-containing seperate constructors for Message model class
    implemented getUserIdsMessageWith method to MessageDao

--- src/app/model/userContent/Message.java
+++ src/app/model/userContent/Message.java
@@ -1,14 +1,38 @@
 package app.model.userContent;
 
+import app.dao.MessageDao;
 import app.model.User;
 
 public class Message extends UserContentItem {
 
     private int receiverId;
 
+    /**
+     * this constructor is used when a new message is to be inserted into database and id is assigned after
+     * the insertion
+     * @param senderId
+     * @param receiverId
+     * @param text
+     */
     public Message(int senderId, int receiverId, String text)
     {
         super(senderId, text);
+        setReceiverId(receiverId);
+        int id = MessageDao.addMessage(this);
+        setID(id);
+    }
+
+    /**
+     * this constructor is for pulling the existing message from the database and when the id is known
+     * @param id
+     * @param senderId
+     * @param receiverId
+     * @param text
+     */
+    public Message(int id, int senderId, int receiverId, String text)
+    {
+        super(senderId, text);
+        setID(id);
         setReceiverId(receiverId);
     }
 
