eeb000e0e78f08e2122713b6bd38c7e458cec5bb
Berkay <146650666+berkaydemircin@users.noreply.github.com>
2024-05-14 22:35:40 +0300

    Added events functionality in Social tab

--- src/app/controller/SocialEventController.java
+++ src/app/controller/SocialEventController.java
@@ -1,13 +1,21 @@
 package app.controller;
 
 
+import app.dao.EventPostDao;
+import app.model.User;
+import app.model.userContent.post.EventPost;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.ToggleButton;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
 
 
 public class SocialEventController {
@@ -18,15 +26,42 @@ public class SocialEventController {
     @FXML
     private Label eventTitle_ID;
 
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader fxmlLoader;
 
+    private EventPost thisEvent;
 
-    @FXML
-    public void unfollowAndRemoveEvent(ActionEvent event) {
-        //that event should be removed and unfollowed from the events page
+    private void switchToFXML(String fxmlFileName, ActionEvent event) throws IOException {
+        fxmlLoader = new FXMLLoader(new File(fxmlFileName).toURI().toURL());
+        Parent root = fxmlLoader.load();
+
+        if (event.getSource() instanceof Node) {                                    //FOR Buttons
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        }
+        else if (event.getSource() instanceof MenuItem menuItem)  {                 //FOR MenuButtons
+            stage = (Stage) menuItem.getParentPopup().getOwnerWindow();
+        }
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setFullScreen(true);     //it should be after stage.setScene
+        stage.show();
+    }
+    public void refresh(ActionEvent event) throws IOException {
+        switchToFXML("src/app/view/SocialPage/socialEventsPage.fxml", event);
     }
 
-    public void setData(EventMock eventMock) {
-        eventTitle_ID.setText( eventMock.getEventTitle());
-        eventTextArea_ID.setText( eventMock.getEventText());
+    @FXML
+    public void unfollowAndRemoveEvent(ActionEvent event) throws IOException
+    {
+        User u = SessionManager.getCurrentUser();
+        EventPostDao.removeFollower(thisEvent.getUserContentItemId(), u.getUserId());
+        refresh(event);
+    }
+
+    public void setData(EventPost event) {
+        thisEvent = event;
+        eventTitle_ID.setText(event.getHeading());
+        eventTextArea_ID.setText(event.getMainText());
     }
 }
