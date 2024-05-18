79f47e1439d33e07a67a8e819708b5be6a01efca
Muhammad Musa Qureshi <145343082+musa-qureshi@users.noreply.github.com>
2024-05-18 17:59:54 +0300

    Minor Visual Edits Made
    - Capitalized prompt texts in certain places
    - Also made the program go to the login page first, instead of the homepage

--- src/app/controller/HomePage.java
+++ src/app/controller/HomePage.java
@@ -13,8 +13,7 @@ import java.net.URL;
 public class HomePage extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        SessionManager.setCurrentUser(UserDao.getUserById(1));
-        FXMLLoader fxmlLoader = new FXMLLoader(new File("src/app/view/HomePage.fxml").toURI().toURL());
+        FXMLLoader fxmlLoader = new FXMLLoader(new File("src/app/view/LoginPage.fxml").toURI().toURL());
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
